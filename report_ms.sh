aa82b7c2f92ab0060548bcd2a3b733a93f7936cf
Jose Renato Ferreira <joserenatosp@outlook.com>
2023-11-23 14:43:07 -0300

    Update report_ms.sh

--- report_ms.sh
+++ report_ms.sh
@@ -1,5 +1,7 @@
 # CHECK PATH / WORKSPACE REPO DISCOVERY RUNNER
 # Shell Task discovery ms
+# Jose Renato ( DevOps Architect )
+# Douglas Oliveira ( Consulting SAS )
 #
 pathtmp=discovery_ms
 
@@ -12,11 +14,13 @@ checkpath(){
 if [ -d "$_workspace" ]; then
         echo "Workspace temp '$_workspace' encontrada, efetuando limpeza ...!"
         rm -rf /tmp/$pathtmp
+        cd /tmp; git clone https://github.com/phahok/discovery_ms.git
 else
         echo "Baixando repo Discovery MS em '$_workspace' ...!"
         echo -e
         echo "Criando WorkSpace Discovery MS "
-        cd /tmp; git clone git@ssh.dev.azure.com:v3/JoseRenatoFerreira/SAS_MS_TEAM/discovery_ms
+        #cd /tmp; git clone git@ssh.dev.azure.com:v3/JoseRenatoFerreira/SAS_MS_TEAM/discovery_ms
+        cd /tmp; git clone https://github.com/phahok/discovery_ms.git
 fi
 }
 
@@ -25,3 +29,9 @@ checkpath
 echo -e "Coletando dados de discovery report em $(hostname) "
 cd $_workspace
 ./Discovery_Main.sh
+
+echo ------ Report finalizado 
+
+echo -e "------ Publicando nova versão de report discovery ms"
+git add Discovery.html; git commit -m "Versão atualizada report discovery gerada em : $(hostname -f)"
+git push -u origin --all
